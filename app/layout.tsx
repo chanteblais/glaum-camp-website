66055d17dceb9eca787fdbc0c033876231fe6d48
chanteblais <chanteblais@gmail.com>
2026-05-15 13:47:52 -0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,15 @@ const libreBaskerville = Libre_Baskerville({
 export const metadata: Metadata = {
   title: 'Glåüm @ What If 2026',
   description: 'Glåüm Theme Camp at What If 2026. Sponsored by Shrimp™. Find your attunement in the forest.',
-  icons: { icon: '/favicon.ico' },
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: ['/favicon/favicon.ico'],
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  manifest: '/favicon/site.webmanifest',
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
